6d6796480da87d126b5a65ff24a1fc2c2521d3b5
ryota-k99 <ninixi16@gmail.com>
2020-01-05 20:11:34 +0900

    first commit

--- CurrencyApp/ViewController.swift
+++ CurrencyApp/ViewController.swift
@@ -7,13 +7,75 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
 
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var countryPicker: UIPickerView!
+    
+    let url = "http://apilayer.net/api/live?access_key=03dc4253384ee909ede5a0af5b62edde&currencies="
+    let countryArray = ["JPY","AUD","SGD","NZD","USD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","MXN","NOK","PLN","RON","RUB","SEK","ZAR"]
+    
+    var country = ""
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        countryArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return countryArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        country = countryArray[row]
+        getPrice(url: url)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        countryPicker.delegate = self
+        countryPicker.dataSource = self
     }
+    
+    func getPrice(url: String){
+        print("succsess")
+        Alamofire.request(url, method: .get).responseJSON{
+            response in
+                if response.result.isSuccess{
+                    let priceJSON: JSON = JSON(response.result.value!)
+                    print("clear")
+                    self.updatePriceData(json: priceJSON)
+                }else{
+                    print(Error.self)
+                    
+                }
+            
+        }
+    }
+    
+    
+    func updatePriceData(json: JSON){
+        
+        if let valueResult = json["quotes"]["USD\(country)"].double {
+                   if let jpyResult = json["quotes"]["USDJPY"].double{
+                       let finalValue = round(jpyResult / valueResult * 1000) / 1000
+                       currencyLabel.text = "\(finalValue)円"
+                       print(country)
+                       countryLabel.text = "1\(country)"
+                   }
+               }else {
+                    currencyLabel.text = "Bitcoin Price not available"
+               }
+               
+           }
+
 
 
 }
