9141e992e08e467682e265f75c94d33dd2845532
ryota-k99 <ninixi16@gmail.com>
2020-01-05 17:46:34 +0900

    Initial Commit

---
+++ CurrencyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyApp
+//
+//  Created by Kato Ryota  on 5/01/20.
+//  Copyright © 2020 Kato Ryota . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
